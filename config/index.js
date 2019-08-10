c7c35c9629ea25a6abca4acb9e5d89f3eae651e2
zhcxk1998 <464203147@qq.com>
2019-08-10 23:41:23 +0800

    chore: 支持async语法.

--- config/index.js
+++ config/index.js
@@ -20,7 +20,8 @@ const config = {
       plugins: [
         'transform-decorators-legacy',
         'transform-class-properties',
-        'transform-object-rest-spread'
+        'transform-object-rest-spread',
+        "transform-runtime",
       ]
     }
   },
