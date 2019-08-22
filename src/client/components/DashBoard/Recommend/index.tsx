629b1095d9f3a01931acbcdc1bdcdfd47a100fea
zhcxk1998 <464203147@qq.com>
2019-08-22 17:17:29 +0800

    feat: 新增推荐链接模块.

---
+++ src/client/components/DashBoard/Recommend/index.tsx
@@ -0,0 +1,27 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Image } from '@tarojs/components'
+
+import './index.scss'
+
+interface IProps {
+
+}
+
+class Recommend extends Component<IProps, {}> {
+  render() {
+    return (
+      <View className='recommend-link-container'>
+        <View className='wrap'>
+          <Image className='img' src='http://cdn.algbb.cn/dashboard/recommend1.png' />
+          <View className='slogan'>康康学堂</View>
+        </View>
+        <View className='wrap'>
+          <Image className='img' src='http://cdn.algbb.cn/dashboard/recommend2.png' />
+          <View className='slogan'>惠惠学堂</View>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default Recommend
