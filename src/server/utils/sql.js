2e6e4cf184e691ad9ce6c847300d945bba8eec65
zhcxk1998 <464203147@qq.com>
2019-08-31 02:26:14 +0800

    chore: 优化sql语句工具函数.

--- src/server/utils/sql.js
+++ src/server/utils/sql.js
@@ -1,6 +1,7 @@
 const QUERY_TABLE = (tableName) => `SELECT * FROM ${tableName};`;
 
-const INSERT_TABLE = (tableName, { key, value }) => `INSERT INTO ${tableName}(${key}) VALUES(${value});`;
+// const INSERT_TABLE = (tableName, { key, value }) => `INSERT INTO ${tableName}(${key}) VALUES(${value});`;
+const INSERT_TABLE = (tableName) => `INSERT INTO ${tableName} SET ?`;
 
 const UPDATE_TABLE = (tableName, { primaryKey, primaryValue }, { key, value }) => `UPDATE ${tableName} SET ${key}=${value} WHERE(${primaryKey}=${primaryValue});`
 
