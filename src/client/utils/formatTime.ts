3bb972a153ff164e367270bf7488951f7ea2024c
zhcxk1998 <464203147@qq.com>
2019-10-17 16:26:11 +0800

    feat: 优化格式化时间的方法.

--- src/client/utils/formatTime.ts
+++ src/client/utils/formatTime.ts
@@ -1,4 +1,4 @@
 export default function formatTime(timeStamp: string) {
   const time = new Date(Number(timeStamp))
-  return time.getHours() + ':' + time.getMinutes()
+  return time.getHours().toString().padStart(2, "0") + ':' + time.getMinutes().toString().padStart(2, "0")
 }
