729e3b78e9e6f4531f163d245c44185a6edcdb94
zhcxk1998 <464203147@qq.com>
2019-11-14 23:21:22 +0800

    feat: 新增首页导航跳转做题界面的链接.

--- src/client/components/Study/Navigation/index.tsx
+++ src/client/components/Study/Navigation/index.tsx
@@ -23,7 +23,7 @@ class Navigation extends Component<IProps, IState> {
   render() {
     const navigator: Array<{ title: string, icon: string, link: string }> = [
       { title: '课程', icon: 'icon-course', link: '/client/pages/courseList/index' },
-      { title: '做题', icon: 'icon-exam', link: '' },
+      { title: '做题', icon: 'icon-exam', link: '/client/pages/exerciseList/index' },
       { title: '分类', icon: 'icon-type', link: '' },
       { title: '排行', icon: 'icon-rank', link: '' }
     ]
