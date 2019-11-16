ea6b495dbfb26ce9feb8f00927e4e5716ab7470a
zhcxk1998 <464203147@qq.com>
2019-11-16 19:15:07 +0800

    feat: 论坛模块接入后端接口.

---
+++ src/client/modals/forumList.ts
@@ -0,0 +1,11 @@
+export interface ForumInfo {
+  forumId: number,
+  forumAvatar: string,
+  forumAuthor: string,
+  publishTime: number,
+  forumImage: string,
+  forumTitle: string,
+  forumContent: string,
+  forumLike: number,
+  forumComment: number
+}
