3819acbf86efa5e20ee4ff81499cc0a06ca86298
zhcxk1998 <464203147@qq.com>
2019-11-17 18:57:28 +0800

    feat: 新增我的帖子空列表页面.

---
+++ src/client/components/Forum/Empty/index.tsx
@@ -0,0 +1,34 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Image } from '@tarojs/components'
+
+import './index.scss'
+
+interface IProps {
+
+}
+
+interface IStates {
+
+}
+
+class Empty extends Component<IProps, IStates>{
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <View className="empty__container">
+        <Image className="image" src="http://cdn.algbb.cn/forum/empty.svg" />
+        <View className="title">No Forums Here</View>
+        <View className="content">赶快前往论坛社区中发表新的贴子吧</View>
+        <Image className="add" src="http://cdn.algbb.cn/forum/add.png" />
+      </View>
+    )
+  }
+}
+
+export default Empty
