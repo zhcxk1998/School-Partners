98c4d121420d0c25289049c27e487246e076e7c6
zhcxk1998 <464203147@qq.com>
2019-11-17 13:54:33 +0800

    feat: 新增按作者名查询论坛内容的接口.

--- src/server/routes/forum.js
+++ src/server/routes/forum.js
@@ -61,4 +61,39 @@ router.get('/forums/:id', async (ctx) => {
   }
 })
 
+router.post('/forums', async (ctx) => {
+  const forumAuthor = ctx.request.body.forumAuthor
+  const res = await query("SELECT * FROM forum_list WHERE BINARY `forum_author` LIKE '%" + forumAuthor + "%'")
+  const isExist = res.length !== 0
+  if (!isExist) {
+    ctx.response.status = 404
+    ctx.response.body = {
+      error: 'forum is not existed'
+    }
+  }
+  else {
+    const response = []
+    res.map((item, index) => {
+      const { forum_id, forum_avatar, forum_author, publish_time, forum_image, forum_title, forum_content, forum_like, forum_comment } = item
+      const timeAgo = computeTimeAgo(publish_time)
+      const date = new Date(Number(publish_time))
+      const publishTimeString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+
+      response[index] = {
+        forumId: forum_id,
+        forumAvatar: forum_avatar,
+        forumAuthor: forum_author,
+        publishTime: publishTimeString,
+        timeAgo,
+        forumImage: forum_image,
+        forumTitle: forum_title,
+        forumContent: forum_content,
+        forumLike: forum_like,
+        forumComment: forum_comment
+      }
+    })
+    ctx.response.body = parse(response);
+  }
+})
+
 module.exports = router
