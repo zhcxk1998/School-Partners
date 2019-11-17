4389a4c9fba7f0ae730712ead4870a975c8b52eb
zhcxk1998 <464203147@qq.com>
2019-11-17 19:01:58 +0800

    feat: 新增论坛删除后端接口.

--- src/server/routes/forum.js
+++ src/server/routes/forum.js
@@ -112,4 +112,9 @@ router.put('/forums', async (ctx) => {
   await query(INSERT_TABLE('forum_list'), data)
 })
 
+router.delete('/forums/:id', async (ctx) => {
+  const { id } = ctx.params
+  await query(DELETE_TABLE('forum_list', { primaryKey: 'forum_id', primaryValue: id }))
+})
+
 module.exports = router
