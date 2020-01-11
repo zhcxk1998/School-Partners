b248b6e24f2568fbacb6569c30a810a4ff4861f5
zhcxk1998 <464203147@qq.com>
2020-01-11 23:43:14 +0800

    feat: 新增获取随机盐值的工具函数

--- src/server/utils/encrypt.js
+++ src/server/utils/encrypt.js
@@ -1,1 +1,11 @@
-module.exports = "zhcxk1998"
+const crypto = require('crypto')
+
+const getRandomSalt = () => {
+  const start = Math.floor(Math.random() * 5)
+  const count = start + Math.ceil(Math.random() * 5)
+  return crypto.randomBytes(10).toString('hex').slice(start, count)
+}
+
+module.exports = {
+  getRandomSalt
+}
