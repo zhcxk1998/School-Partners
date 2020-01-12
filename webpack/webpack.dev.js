26da46946f2e7047f6347a2092e6aa99725b7e82
zhcxk1998 <464203147@qq.com>
2020-01-12 22:49:01 +0800

    chore: 新增管理端接口代理

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -14,6 +14,14 @@ module.exports = merge(common, {
     inline: true,            //启用内联模式，一段处理实时重载的脚本被插入到bundle中，并且构建消息会出现在浏览器控制台
     historyApiFallback: true,//开发单页应用时有用，依赖于HTML5 history API，设为true时所有跳转将指向index.html
     historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        pathRewrite: { '^/api': '' },
+        changeOrigin: true,
+        secure: false,
+      },
+    }
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),  //webpack内置的热更新插件
