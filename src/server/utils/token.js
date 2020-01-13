3df6dfad494983216175d1af630985af8700be63
zhcxk1998 <464203147@qq.com>
2020-01-13 09:50:16 +0800

    feat: 新增管理端获取、生成token工具函数

---
+++ src/server/utils/token.js
@@ -0,0 +1,16 @@
+const secret = "zhcxk1998"
+
+const jwt = require('jsonwebtoken')
+
+const generateToken = (payload = {}) => (
+  'Bearer ' + jwt.sign(payload, secret, { expiresIn: '1d' })
+)
+
+const getJWTPayload = (token) => (
+  jwt.verify(token.split(' ')[1], secret)
+)
+
+module.exports = {
+  generateToken,
+  getJWTPayload
+}
