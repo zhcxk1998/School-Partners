20707cd828a2aba293b34a7a4a6fe50c2714d8de
zhcxk1998 <464203147@qq.com>
2020-01-25 20:17:13 +0800

    feat: 新增管理端组件整合文件

---
+++ src/admin/components/index.tsx
@@ -0,0 +1,21 @@
+import ContentMain from './ContentMain'
+import CustomBreadcrumb from './CustomBreadcrumb'
+import Footer from './Footer'
+import HeaderNav from './HeaderNav'
+import LinkIcon from './LinkIcon'
+import LoadableComponent from './LoadableComponent'
+import Main from './Main'
+import PrivateRoute from './PrivateRoute'
+import SiderNav from './SiderNav'
+
+export {
+  ContentMain,
+  CustomBreadcrumb,
+  Footer,
+  HeaderNav,
+  LinkIcon,
+  LoadableComponent,
+  Main,
+  PrivateRoute,
+  SiderNav
+}
