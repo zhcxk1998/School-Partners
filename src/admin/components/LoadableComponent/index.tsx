904be7551130b94c60c900f5bbfffe1a48def1e0
zhcxk1998 <464203147@qq.com>
2020-01-26 22:25:14 +0800

    refactor: 使用hook重构懒加载组件

--- src/admin/components/LoadableComponent/index.tsx
+++ src/admin/components/LoadableComponent/index.tsx
@@ -1,26 +1,21 @@
-import React, { Component } from 'react'
+import React, { useEffect, FC } from 'react'
 import Loadable from 'react-loadable'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 
-class LoadingPage extends Component {
-  // 类似github页面加载的那个加载条
-  componentDidMount() {
+const LoadingPage: FC = () => {
+  useEffect(() => {
     NProgress.start()
-  }
-
-  componentWillUnmount() {
-    NProgress.done()
-  }
-
-  render() {
-    return (
-      <div className="load-component" />
-    )
-  }
+    return () => {
+      NProgress.done()
+    }
+  }, [])
+  return (
+    <div className="load-component" />
+  )
 }
 
-const LoadableComponent = (component: any) => Loadable({
+const LoadableComponent = (component: () => Promise<any>) => Loadable({
   loader: component,
   loading: () => <LoadingPage />,
 })
