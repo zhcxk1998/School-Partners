2be9a3f46909f84bf0da14644e38ccffed9c99f3
zhcxk1998 <464203147@qq.com>
2020-01-27 22:05:58 +0800

    style: 修改管理端面包屑导航栏样式

--- src/admin/components/CustomBreadcrumb/index.tsx
+++ src/admin/components/CustomBreadcrumb/index.tsx
@@ -3,7 +3,7 @@ import { Breadcrumb } from 'antd'
 import { Link } from 'react-router-dom'
 
 const CustomBreadcrumb = ({ list }: { list: Array<string> }) => (
-  <Breadcrumb style={{ marginBottom: 16 }}>
+  <Breadcrumb style={{ marginBottom: 20, height: 20 }}>
     <Breadcrumb.Item><Link to='/admin'>首页</Link></Breadcrumb.Item>
     {list && list.map((item: string) => {
       return <Breadcrumb.Item key={item}>{item}</Breadcrumb.Item>
