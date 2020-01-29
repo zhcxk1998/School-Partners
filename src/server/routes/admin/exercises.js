4bf871aff0fb7d66db411c844198d6caf365e814
zhcxk1998 <464203147@qq.com>
2020-01-29 11:01:51 +0800

    feat: 新增管理端题库批量删除的后台接口

--- src/server/routes/admin/exercises.js
+++ src/server/routes/admin/exercises.js
@@ -60,4 +60,25 @@ router.delete('/exercises/:id', async (ctx) => {
   }
 })
 
+router.delete('/exercises', async (ctx) => {
+  const deleteIdList = ctx.request.body
+  const responseBody = {
+    code: 0,
+    data: {}
+  }
+  try {
+    await Promise.all(deleteIdList.map(async (deleteId) => {
+      await query(`DELETE FROM exercise_list WHERE id = ${deleteId}`)
+    }))
+    responseBody.data.msg = '删除成功'
+    responseBody.code = 200
+  } catch (e) {
+    responseBody.data.msg = '无此课程'
+    responseBody.code = 404
+  } finally {
+    ctx.response.status = responseBody.code
+    ctx.response.body = responseBody
+  }
+})
+
 module.exports = router
