b781ee9fe637a068b8e7f0623f54f8d830f43481
zhcxk1998 <464203147@qq.com>
2020-01-29 11:01:22 +0800

    feat: 新增管理端获取用户信息的后台接口

--- src/server/routes/admin/info.js
+++ src/server/routes/admin/info.js
@@ -3,8 +3,14 @@ const { getJWTPayload } = require('../../utils/token')
 
 router.get('/info', async (ctx) => {
   const payload = getJWTPayload(ctx.headers.authorization)
+  const { username } = payload
   ctx.response.status = 200
-  ctx.response.body = payload
+  ctx.response.body = {
+    code: 200,
+    data: {
+      username
+    }
+  }
 })
 
 module.exports = router
