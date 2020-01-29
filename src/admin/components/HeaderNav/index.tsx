d4d0bee8b3ba9a27f0144196d9f0555ade526324
zhcxk1998 <464203147@qq.com>
2020-01-29 11:00:47 +0800

    feat: 新增管理端头部导航栏显示用户信息

--- src/admin/components/HeaderNav/index.tsx
+++ src/admin/components/HeaderNav/index.tsx
@@ -1,28 +1,47 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { withRouter, RouteComponentProps } from 'react-router-dom'
-import { Menu, Dropdown, Icon } from 'antd'
+import { Menu, Dropdown, Icon, message } from 'antd'
+import http from '@/admin/utils/http'
 
 import './index.scss'
 
-const menu = (
-  <Menu>
-    <Menu.Item>
-      <Icon type="user" />
-      个人信息
-    </Menu.Item>
-    <Menu.Item>
-      <Icon type="setting" />
-      管理设置
-    </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item>
-      <Icon type="poweroff" />
-      退出登录
-    </Menu.Item>
-  </Menu>
-)
+const HeaderNav: FC<RouteComponentProps> = (props: RouteComponentProps) => {
+  const [username, setUsername] = useState<string>('')
 
-const HeaderNav: FC<RouteComponentProps> = () => {
+  useEffect(() => {
+    getUserInfo()
+  }, [])
+
+  const getUserInfo = async () => {
+    const { data: { username } } = await http.get('/info')
+    setUsername(username)
+  }
+
+  const handleLogout = () => {
+    const { history } = props
+    message.success('退出成功')
+    localStorage.removeItem('token')
+    history.push('/login')
+  }
+
+  const menu = (
+    <Menu>
+      <Menu.Item>
+        <Icon type="user" />
+        个人信息
+      </Menu.Item>
+      <Menu.Item>
+        <Icon type="setting" />
+        管理设置
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item onClick={handleLogout}>
+        <Icon type="poweroff" />
+        退出登录
+      </Menu.Item>
+    </Menu>
+  )
+
   return (
     <div className="header__container">
       <div className="header__wrap">
@@ -31,7 +50,7 @@ const HeaderNav: FC<RouteComponentProps> = () => {
         </div>
         <Dropdown overlay={menu}>
           <div className="info">
-            <span>zhcxk1998</span>
+            <span>{username}</span>
             <img src="http://cdn.algbb.cn/avatar" width="35" height="35" />
           </div>
         </Dropdown>
