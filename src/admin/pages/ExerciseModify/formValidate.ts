d489d3ad477632cb6b13799cfe6206385dfe0b3f
zhcxk1998 <464203147@qq.com>
2020-01-31 09:39:56 +0800

    feat: 新增管理端修改题库的表单验证

---
+++ src/admin/pages/ExerciseModify/formValidate.ts
@@ -0,0 +1,31 @@
+export const ExerciseNameRules = [
+  { required: true, message: '请输入题库名称' }
+]
+
+export const ExerciseContentRules = [
+  { required: true, message: '请输入题库简介' }
+]
+
+export const ExerciseDifficultyRules = [
+  { required: true, message: '请选择题库难度' }
+]
+
+export const ExerciseTypeRules = [
+  { required: true, message: '请选择题库类型' }
+]
+
+export const TopicContentRules = [
+  { required: true, message: '请选择题目内容' }
+]
+
+export const TopicTypeRules = [
+  { required: true, message: '请选择题目类型' }
+]
+
+export const TopicOptionRules = [
+  { required: true, message: '请填写题目选项' }
+]
+
+export const TopicAnswerRules = [
+  { required: true, message: '请选择题目正确答案' }
+]
