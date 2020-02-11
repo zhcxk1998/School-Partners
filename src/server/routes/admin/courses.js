a61ddffdb6155903c7a5a3226a7e06374d4f89a8
zhcxk1998 <464203147@qq.com>
2020-02-11 10:44:45 +0800

    feat: 新增管理端课程修改的后台接口

--- src/server/routes/admin/courses.js
+++ src/server/routes/admin/courses.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
 const { query } = require('../../utils/query')
-const { QUERY_TABLE, INSERT_TABLE } = require('../../utils/sql');
+const { QUERY_TABLE, INSERT_TABLE, REPLACE_TABLE } = require('../../utils/sql');
 const parse = require('../../utils/parse')
 
 router.get('/courses', async (ctx) => {
@@ -27,6 +27,37 @@ router.get('/courses', async (ctx) => {
       courseList: parse(responseData),
       total: responseData.length
     }
+  } catch (e) {
+    responseBody.code = 404
+    responseBody.data = {
+      msg: '无课程信息'
+    }
+  } finally {
+    ctx.response.status = responseBody.code
+    ctx.response.body = responseBody
+  }
+})
+
+router.get('/courses/:id', async (ctx) => {
+  const { id: courseId } = ctx.params
+  const responseBody = {
+    code: 0,
+    data: {}
+  }
+  try {
+    const res = await query(`SELECT * FROM course_list WHERE id = ${courseId}`);
+    const { id, course_name, is_recommend, course_author, publish_date, course_description, course_rate, course_steps } = res[0]
+    responseBody.data = {
+      id,
+      courseName: course_name,
+      isRecommend: !!is_recommend,
+      publishDate: parseInt(publish_date) || 0,
+      courseAuthor: course_author,
+      courseDescription: course_description,
+      courseRate: course_rate,
+      courseSteps: course_steps ? JSON.parse(course_steps) : []
+    }
+    responseBody.code = 200
   } catch (e) {
     responseBody.code = 404
     responseBody.data = {
@@ -106,6 +137,45 @@ router.delete('/courses', async (ctx) => {
   } catch (e) {
     responseBody.data.msg = '无此课程'
     responseBody.code = 404
+  } finally {
+    ctx.response.status = responseBody.code
+    ctx.response.body = responseBody
+  }
+})
+
+router.put('/courses/:id', async (ctx) => {
+  const { id: courseId } = ctx.params
+  const {
+    courseName,
+    courseDescription,
+    courseAuthor,
+    courseRate,
+    isRecommend,
+    stepList
+  } = ctx.request.body
+  const responseBody = {
+    code: 0,
+    data: {}
+  }
+  try {
+    const res = await query(`SELECT * FROM course_list WHERE id = ${courseId}`);
+    const { publish_date, course_views } = res[0]
+    await query(REPLACE_TABLE('course_list'), {
+      id: courseId,
+      course_name: courseName,
+      course_description: courseDescription,
+      course_author: courseAuthor,
+      course_rate: courseRate,
+      is_recommend: isRecommend,
+      course_steps: JSON.stringify(stepList),
+      publish_date,
+      course_views
+    })
+    responseBody.data.msg = '修改成功'
+    responseBody.code = 200
+  } catch (e) {
+    responseBody.data.msg = '异常错误'
+    responseBody.code = 500
   } finally {
     ctx.response.status = responseBody.code
     ctx.response.body = responseBody
