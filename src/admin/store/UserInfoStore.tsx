70cfeb3e0b2df8eb65bf5adfc67de5e59c5c03cc
zhcxk1998 <464203147@qq.com>
2020-04-09 13:48:41 +0800

    feat: 新增管理端全局用户信息类

---
+++ src/admin/store/UserInfoStore.tsx
@@ -0,0 +1,20 @@
+import { action, observable, configure, runInAction } from 'mobx';
+import http from '@/admin/utils/http'
+
+configure({ enforceActions: 'observed' });
+
+class UserInfoStore {
+  @observable username = ''
+  @observable isActived = false
+
+  @action.bound
+  async setUserInfo() {
+    const { data: { username, isActived } } = await http.get('/info')
+    runInAction(() => {
+      this.username = username
+      this.isActived = isActived
+    })
+  }
+}
+
+export default UserInfoStore;
