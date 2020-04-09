6ac22aeb3ecd51ad7f7ba24461d3e8a2fabb8532
zhcxk1998 <464203147@qq.com>
2020-04-09 13:46:52 +0800

    feat: 新增管理端token添加用户id

--- src/server/routes/admin/info.js
+++ src/server/routes/admin/info.js
@@ -1,14 +1,17 @@
 const router = require('koa-router')()
 const { getJWTPayload } = require('../../utils/token')
+const { query } = require('../../utils/query')
 
 router.get('/info', async (ctx) => {
   const payload = getJWTPayload(ctx.headers.authorization)
-  const { username } = payload
+  const { username, userId } = payload
+  const { is_actived } = (await query(`select is_actived from user_info where id = ${userId}`))[0]
   ctx.response.status = 200
   ctx.response.body = {
     code: 200,
     data: {
-      username
+      username,
+      isActived: !!is_actived
     }
   }
 })
